c3eda67b91456507ad50e715581bfc2fcc3c0c30
Eric Chesbrough <ericchesbrough@gmail.com>
2013-11-02 22:59:46 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puppysesh::Application.config.secret_key_base = '9b09d4cf6d6080e62fe6243279bd848e752cd8aabfe2cc729950d9b8d2e5606442bbb2e89f56b99ea124877c0cb53d9d94093c285a1d840fa3ae69edeb335bc0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Puppysesh::Application.config.session_store :cookie_store, key: '_puppysesh_session'
